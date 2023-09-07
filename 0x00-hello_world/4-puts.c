811bdccd019a4e77a2856744d54615374d23c9a3
Your Name <christopheremelifejnr@gmail.com>
2023-09-07 13:14:25 -0700

    C hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    // Use the puts function to print the message
     puts("Programming is like building a multilingual puzzle");
-
-    // Return 0 to end the program
     return 0;
 }
