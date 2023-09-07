641543c7511c5d162a2653d412974a8c6d9a87b8
Your Name <christopheremelifejnr@gmail.com>
2023-09-07 13:09:56 -0700

    C hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,9 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle"), return 0;
+#include <stdio.h>
+
+int main() {
+    // Use the puts function to print the message
+    puts("Programming is like building a multilingual puzzle");
+
+    // Return 0 to end the program
+    return 0;
+}
