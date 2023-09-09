2cf59d73834bff0bf4145ce10f781d94ead3c533
Your Name <christopheremelifejnr@gmail.com>
2023-09-08 18:35:02 -0700

    C variables

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,7 +10,7 @@ int digit1, digit2, digit3;
 
 for (digit1 = 0; digit1 <= 7; digit1++)
 {
-for (digit2 = digit1 + 1; digit2 <= 9; digit2++)
+for (digit2 = digit1 + 1; digit2 <= 8; digit2++)
 {
 for (digit3 = digit2 + 1; digit3 <= 9; digit3++)
 {
@@ -20,8 +20,10 @@ putchar('0' + digit3);
 
 
 if (digit1 < 7 || digit2 < 8 || digit3 < 9)
+{
 putchar(',');
 putchar(' ');
+}
 }
 }
 }
