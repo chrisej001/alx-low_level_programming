1c0b388210c1783923d842b0864e74c134afdea7
Your Name <christopheremelifejnr@gmail.com>
2023-09-13 08:28:45 -0700

    C nest function

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -4,21 +4,23 @@
 * main - Prints the sum of the even-valued terms, followed by a new line.
 * Return: Always 0.
 */
+
 int main(void)
 {
-long fib1 = 1;
-long fib2 = 2;
+long fib1 = 0;
+long fib2 = 1;
 
 printf("%ld\n", fib1);
 
-int i;
+int i = 1;
 for (; i < 10; i++)
 {
 long next_term = fib1 + fib2;
-printf("%ld\n", next_term);
+printf("%ld\n", next_term)
 fib1 = fib2;
 fib2 = next_term;
 }
+
 
 return (0);
 }
