7f4f1270e68e8455ab2d01dc702681b2ed69e527
Your Name <christopheremelifejnr@gmail.com>
2023-09-13 07:54:29 -0700

    C nest function

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main - Prints the sum of the even-valued terms, followed by a new line.
+* Return: Always 0.
+*/
+int main(void)
+{
+long fib1 = 1;
+long fib2 = 2;
+long sum = 0;
+
+while (fib2 <= 4000000)
+{
+if (fib2 % 2 == 0)
+{
+sum += fib2;
+}
+
+long next_term = fib1 + fib2;
+fib1 = fib
+fib2 = next_term;
+}
+
+printf("Sum of even-valued Fibonacci terms not exceeding 4,000,000: %ld\n",
+sum);
+
+return (0);
+}
